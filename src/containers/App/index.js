1418459588ae1e74c6ceac2ad1f0a1bfa4763634
zhangwendong <wond-z@live.com>
2020-10-21 16:38:29 +0800

    :heavy_plus_sign: 引入Ant Design组件库，并创建Menus组件（导航）

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 // import style from './App.css';
 import './index.css';
 import Header from '../../components/Header'
+import Menus from '../../components/Menus'
 
 class Home extends Component {
     render() {
         return (
             <div className="container">
                 <Header />
+                <div className="nav"><Menus /></div>
             </div>
         )
     }
