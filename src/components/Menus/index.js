df69de2202b62c6743b74d086e2dbb9337c0eb06
zhangwendong <wond-z@live.com>
2020-10-26 18:27:34 +0800

    :heavy_plus_sign: 添加font-awesome4.7.0

--- src/components/Menus/index.js
+++ src/components/Menus/index.js
@@ -1,21 +1,29 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
 import { Menu } from 'antd';
-import { SmileOutlined } from '@ant-design/icons';
+// import { SmileOutlined } from '@ant-design/icons';
+// import { createFromIconfontCN } from '@ant-design/icons';
+import faStyles from 'font-awesome/css/font-awesome.css';
 import 'antd/dist/antd.css';
 
 const menus = [
 	{
         key: '/',
 		title: '添加角色',
-		icon: 'page'
+		icon: 'plus-square-o'
 
 	}, {
         key: '/iOS',
 		title: '删除角色',
-		icon: 'message'
+		icon: 'minus-square-o'
 	}
-]
+];
+
+// 使用createFromIconfontCN方式引入（依赖网络环境）
+// const IconFont = createFromIconfontCN({
+// 	// 通过iconfont.cn生成
+// 	scriptUrl: '//at.alicdn.com/t/font_2158544_70d674n5yjq.js',
+// });
 
 export default class Menus extends Component {
     constructor(props) {
@@ -30,8 +38,8 @@ export default class Menus extends Component {
 		return (
 			<Menu.Item key={key}>
 				<Link to={key}>
-					{icon && <SmileOutlined type={icon}/>}
-					<span>{title}</span>
+					{/* {icon && <SmileOutlined type={icon}/>} */}
+					{icon && <i class={'fa fa-' + icon}></i>} <span>{title}</span>
 				</Link>
 			</Menu.Item>
 		)
--- src/pages/AddUser/index.js
+++ src/pages/AddUser/index.js
@@ -1,10 +1,60 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
+import {Form, Input, Button, Radio, Select, Checkbox} from 'antd';
+import {SettingOutlined} from '@ant-design/icons';
+const {Option} = Select;
+
+const layout = {
+    labelCol: {
+        span: 8
+    },
+    wrapperCol: {
+        span: 16
+    }
+};
+const tailLayout = {
+    wrapperCol: {
+        offset: 8,
+        span: 16
+    }
+};
 
 class AddUser extends Component {
     render() {
-        return(
-            <h1>首页</h1>
-        )
+        const onFinish = (values) => {
+            console.log('Success:', values);
+        };
+
+        const onFinishFailed = (errorInfo) => {
+            console.log('Failed:', errorInfo);
+        };
+
+        return (<Form {...layout} name="basic" initialValues={{
+                remember: true
+            }} onFinish={onFinish} onFinishFailed={onFinishFailed}>
+            <Form.Item label="用户名" name="username" rules={[{
+                        required: true,
+                        message: '请输入用户！'
+                    }
+                ]}>
+                <Input/>
+            </Form.Item>
+
+            <Form.Item label="密码" name="password" rules={[{
+                        required: true,
+                        message: '请输入密码！'
+                    }
+                ]}>
+                <Input.Password/>
+            </Form.Item>
+
+            <Form.Item {...tailLayout} name="remember" valuePropName="checked">
+                <Checkbox>记住我</Checkbox>
+            </Form.Item>
+
+            <Form.Item {...tailLayout}>
+                <Button type="primary" htmlType="submit">提交</Button>
+            </Form.Item>
+        </Form>)
     }
 }
 
